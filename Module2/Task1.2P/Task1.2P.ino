6ddaffe7d56f824ee96469601a1860379a8c7221
rcallow <rcallow@deakin.edu.au>
2019-03-09 12:29:58 +1000

    Create Task1.2P.ino

---
+++ Module2/Task1.2P/Task1.2P.ino
@@ -0,0 +1,36 @@
+int inputPin = 2;
+//int lightLevel = 0;
+bool isLevel = 0;
+
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(inputPin, INPUT);
+  Serial.begin(9600);
+  Serial.println("Anduino light sensing started\n");
+}
+
+void loop()
+{
+  isLevel = digitalRead(inputPin);
+
+  if(isLevel)
+  {
+    digitalWrite(13, HIGH);
+    Serial.println("LED on");
+  }
+  else
+  {
+    digitalWrite(13, LOW);
+    Serial.println("LED off");
+  }
+  if(isLevel)
+  {
+    Serial.print("Arduino is not tilted");
+  }
+  else
+  {
+    Serial.println("Arduino is tilted");
+  }
+  delay(1000);
+}
