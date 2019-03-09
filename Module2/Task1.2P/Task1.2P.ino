5d45d758591748eb64a943e2d7a2520b52e4589a
rcallow <rcallow@deakin.edu.au>
2019-03-09 12:48:46 +1000

    Fixed errors

--- Module2/Task1.2P/Task1.2P.ino
+++ Module2/Task1.2P/Task1.2P.ino
@@ -1,14 +1,14 @@
 int inputPin = 2;
 //int lightLevel = 0;
 bool isLevel = 0;
-byte ledStatus = LOW;
+volatile byte ledStatus = LOW;
 
 void setup()
 {
   pinMode(13, OUTPUT);
   pinMode(inputPin, INPUT);
   
-  attachInterrupt(digitalPinToInterrupt(inputPin), tiltResponse, FALLING);
+  attachInterrupt(digitalPinToInterrupt(inputPin), tiltResponse, CHANGE);
   
   Serial.begin(9600);
   Serial.println("Anduino tilt sensing started\n");
@@ -21,5 +21,5 @@ void loop()
 
 void tiltResponse()
 {
-  ledStatus = HIGH;
+  ledStatus = !ledStatus;
 }
