4cad30dbcf9283159d84f5240ebad67b87d6a486
rcallow <rcallow@deakin.edu.au>
2019-03-13 12:59:17 +1000

    Update Task1.1P.ino

--- Module1/Task1.1P/Task1.1P.ino
+++ Module1/Task1.1P/Task1.1P.ino
@@ -1,13 +1,14 @@
-int inputPin = 12;
-//int lightLevel = 0;
+uint8_t inputPin = 2;
+uint8_t outputPin = 13;
+bool changeDetected = 0;
 bool isLevel = 0;
 
 void setup()
 {
-  pinMode(13, OUTPUT);
-  pinMode(12, INPUT);
+  pinMode(outputPin, OUTPUT);
+  pinMode(inputPin, INPUT);
   Serial.begin(9600);
-  Serial.println("Anduino light sensing started\n");
+  Serial.println("Anduino tilt sensing started");
 }
 
 void loop()
