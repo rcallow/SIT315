dab4367621b7c30abd659aaf080bb91b9bb3d403
rcallow <rcallow@deakin.edu.au>
2019-03-13 14:08:17 +1000

    Update Task1.2P.ino

--- Module1/Task1.2P/Task1.2P.ino
+++ Module1/Task1.2P/Task1.2P.ino
@@ -1,33 +1,42 @@
-int inputPin = 2;
+uint8_t inputPin = 2;
+uint8_t outputPin = 13;
 volatile bool isLevel = 1;
+volatile bool changeDetected = 0;
+
 
 void setup()
 {
-  pinMode(13, OUTPUT);
+  pinMode(outputPin, OUTPUT);
   pinMode(inputPin, INPUT);
   
   attachInterrupt(digitalPinToInterrupt(inputPin), tiltResponse, CHANGE);
   
   Serial.begin(9600);
-  Serial.println("Anduino tilt sensing started\n");
+  Serial.println("Anduino tilt sensing started");
 }
+
 
 void loop()
 {
-  if(isLevel)
+  if(changeDetected)
   {
-    digitalWrite(13, LOW);
-    Serial.println("Anduino is level");
+    if(isLevel)
+    {
+      digitalWrite(outputPin, HIGH);
+      Serial.println("Anduino is level");
+    }
+    else
+    {
+      digitalWrite(outputPin, LOW);
+      Serial.println("Anduino is tilted");
+    }
+  changeDetected = 0;
   }
-  else
-  {
-    digitalWrite(13, HIGH);
-    Serial.println("Anduino is tilted");
-  }
-  delay(1000);
 }
+
 
 void tiltResponse()
 {
+  changeDetected = 1;
   isLevel = digitalRead(inputPin);
 }
