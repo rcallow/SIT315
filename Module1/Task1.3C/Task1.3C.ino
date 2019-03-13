9347271c7098be97eed5233d016b143dc00c11a5
rcallow <rcallow@deakin.edu.au>
2019-03-13 16:42:36 +1000

    Added comments to 1.3C

--- Module1/Task1.3C/Task1.3C.ino
+++ Module1/Task1.3C/Task1.3C.ino
@@ -14,7 +14,8 @@ void setup()
   pinMode(widthOutputPin, OUTPUT);
   pinMode(lengthTiltPin, INPUT);
   pinMode(widthTiltPin, INPUT);
-  
+
+  // Attach interrupts to both tilt sensors
   attachInterrupt(digitalPinToInterrupt(lengthTiltPin), lengthTiltResponse, CHANGE);
   attachInterrupt(digitalPinToInterrupt(widthTiltPin), widthTiltResponse, CHANGE);
   
@@ -24,44 +25,54 @@ void setup()
 
 void loop()
 {
+  // If length-tilt interrupt has occurred
   if(lengthChangeDetected)
   {
     if(lengthIsLevel)
     {
+      // Turn off yellow LED
       digitalWrite(lengthOutputPin, LOW);
       Serial.println("Arduino length is level");
     }
     else
     {
+      // Turn on yellow LED
       digitalWrite(lengthOutputPin, HIGH);
       Serial.println("Arduino length is tilted");
     }
+    // Reset length interrupt detector
     lengthChangeDetected = 0;
   }
+  // If width-tilt interrupt has occurred
   if(widthChangeDetected)
   {
     if(widthIsLevel)
     {
+      // Turn on blue LED
       digitalWrite(widthOutputPin, LOW);
       Serial.println("Arduino width is level");
     }
     else
     {
+      // Turn on blue LED
       digitalWrite(widthOutputPin, HIGH);
       Serial.println("Arduino width is tilted");
     }
+    // Reset width interrupt detector
     widthChangeDetected = 0;
   }
 }
 
 void lengthTiltResponse()
 {
+  // Set length interrupt detector
   lengthChangeDetected = 1;
   lengthIsLevel = digitalRead(lengthTiltPin);
 }
 
 void widthTiltResponse()
 {
+  // Set width interrupt detector
   widthChangeDetected = 1;
   widthIsLevel = digitalRead(widthTiltPin);
 }
