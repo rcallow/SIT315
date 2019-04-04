1759a8c4b1232425a601c3771fcd3fad7e78832d
kokoro <kokoro@kokoro-Latitude-E4200>
2019-04-04 23:13:47 +1000

    Added timer

--- Module2/M2T1P_V2/main.cpp
+++ Module2/M2T1P_V2/main.cpp
@@ -4,19 +4,24 @@
 #include <iostream>
 #include <iomanip>
 #include "Matrix.h"
+#include "time.h"
+#include<stdio.h>
 
 using namespace std;
 
-const int N = 10;
+const int N = 470;
 
 int main()
 {
     Matrix A = Matrix(N);
     Matrix B = Matrix(N);
     Matrix C = Matrix(N);
+    
+    double time_elapsed = 0.0;
 
     //Start timing here
-
+    //measuring time -- using clock
+    long clock_start = clock();
 
     for (int rowOfA = 0; rowOfA < N; ++rowOfA)
     {
@@ -30,15 +35,21 @@ int main()
             C.setValue(rowOfA, columnOfB, result);	
 	}
     }
+    
+    long clock_end = clock();
+    time_elapsed = (clock_end - clock_start) / CLOCKS_PER_SEC;
 
-    cout << "Matrix A:" << endl << endl;
-    A.printMatrix();
-    cout << endl << endl << "Matrix B:" << endl << endl;
-    B.printMatrix();
-    cout << endl << endl << "Matrix C:" << endl << endl;
-    C.printMatrix();
+    printf("Time elapsed: %.2f\n", time_elapsed);
+//    cout << "Matrix A:" << endl << endl;
+//    A.printMatrix();
+//    cout << endl << endl << "Matrix B:" << endl << endl;
+//    B.printMatrix();
+//    cout << endl << endl << "Matrix C:" << endl << endl;
+//    C.printMatrix();
+
 }
 //Output time here
+
 //Write output to file
 //Format output into columns
 //Add comments, error checking, exception handling
