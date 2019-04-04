7f9b42ed5645eb9ed51e4a09446581d411624840
kokoro <kokoro@kokoro-Latitude-E4200>
2019-04-05 00:44:25 +1000

    Added loop algorithm

--- Module2/M2T1P_pthread/main.cpp
+++ Module2/M2T1P_pthread/main.cpp
@@ -12,12 +12,27 @@ using namespace std;
 
 const int N = 470;
 const int NUMBER_OF_THREADS = 2;
+int rowsPerThread = 0;
+Matrix A;
+Matrix B;
+Matrix C;
 
 
 void *MultiplyPartMatrices(void *id)
 {
-   int threadId = (int)id;
- 
+    int threadId = (int)id;
+    for (int rowOfA = rowsPerThread * threadId; rowOfA < (rowsPerThread * threadId + rowsPerThread); ++rowOfA)
+    {
+        for (int columnOfB = 0; columnOfB < N; ++columnOfB)
+        {
+            int result = 0;
+            for (int i = 0; i < N; ++i)
+            {
+                result = result + A.getValue(rowOfA, i) * B.getValue(i, columnOfB);		
+            }
+            C.setValue(rowOfA, columnOfB, result);	
+	}
+    }
    pthread_exit(NULL);
 }
 
@@ -26,12 +41,11 @@ void *MultiplyPartMatrices(void *id)
 int main()
 {
     pthread_t threads[NUMBER_OF_THREADS];
-    Matrix A = Matrix(N);
-    Matrix B = Matrix(N);
-    Matrix C = Matrix(N);
+    A = Matrix(N);
+    B = Matrix(N);
+    C = Matrix(N);
     
     int remainder = 0;
-    int rowsPerThread = 0;
     double time_elapsed = 0.0;
 
     //Start timing here
@@ -39,7 +53,7 @@ int main()
     long clock_start = clock();
 
     remainder = N % NUMBER_OF_THREADS;
-    NUMBER_OF_THREADS -= remainder;
+    N -= remainder;
     rowsPerThread = N / NUMBER_OF_THREADS;
     
     for (int rowOfA = 0; rowOfA < N; ++rowOfA)
