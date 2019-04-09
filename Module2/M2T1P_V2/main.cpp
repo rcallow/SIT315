146c97ccfbf823afb3d5b04b761684d6eaa858d3
rcallow <rcallow87@gmail.com>
2019-04-09 22:58:16 +1000

    Removed Matrix class

--- Module2/M2T1P_V2/main.cpp
+++ Module2/M2T1P_V2/main.cpp
@@ -2,54 +2,89 @@
 //
 
 #include <iostream>
-#include <iomanip>
-#include "Matrix.h"
-#include "time.h"
-#include<stdio.h>
+#include <stdio.h>
+#include <sys/time.h>
 
 using namespace std;
 
-const int N = 470;
+
+const int MATRIX_SIZE = 800;
+
+int matrixA[MATRIX_SIZE][MATRIX_SIZE];
+int matrixB[MATRIX_SIZE][MATRIX_SIZE];
+int matrixC[MATRIX_SIZE][MATRIX_SIZE];
+
+
 
 int main()
 {
-    Matrix A = Matrix(N);
-    Matrix B = Matrix(N);
-    Matrix C = Matrix(N);
-    
     double time_elapsed = 0.0;
+    
+    srand (time (0));
+    for (int i = 0; i < MATRIX_SIZE; i++)
+        {
+        for (int j = 0; j < MATRIX_SIZE; j++)
+        {
+            matrixA[i][j] = rand() % 4;
+            matrixB[i][j] = rand() % 4;
+        }
+    }
 
-    //Start timing here
-    //measuring time -- using clock
-    long clock_start = clock();
 
-    for (int rowOfA = 0; rowOfA < N; ++rowOfA)
+    cout << endl << endl << "Before loop A: " << endl;
+    for (int i = 0; i < MATRIX_SIZE; i++)
     {
-        for (int columnOfB = 0; columnOfB < N; ++columnOfB)
+        for (int j = 0; j < MATRIX_SIZE; j++)
+        {
+            //    cout << matrixA[i][j];
+        }
+        //  cout << endl;
+    }
+       
+    cout << endl << endl << "Before loop B: " << endl;
+    for (int i = 0; i < MATRIX_SIZE; i++)
+    {
+        for (int j = 0; j < MATRIX_SIZE; j++)
+        {
+            //    cout << matrixB[i][j];
+        }
+        //cout << endl;
+    }
+               
+    cout << endl <<  endl;
+    
+    
+    struct timeval timecheck;
+    gettimeofday(&timecheck, NULL);
+    long timeofday_start = (long)timecheck.tv_sec * 1000 + (long)timecheck.tv_usec / 1000;
+       
+
+    for (int rowOfA = 0; rowOfA < MATRIX_SIZE; ++rowOfA)
+    {
+        for (int columnOfB = 0; columnOfB < MATRIX_SIZE; ++columnOfB)
         {
             int result = 0;
-            for (int i = 0; i < N; ++i)
+            for (int i = 0; i < MATRIX_SIZE; ++i)
             {
-                result = result + A.getValue(rowOfA, i) * B.getValue(i, columnOfB);		
+                result = result + matrixA[rowOfA][i] * matrixB[i][columnOfB];		
             }
-            C.setValue(rowOfA, columnOfB, result);	
+            matrixC[rowOfA][columnOfB] = result;
 	}
     }
     
-    long clock_end = clock();
-    time_elapsed = (clock_end - clock_start) / CLOCKS_PER_SEC;
-
-    printf("Time elapsed: %.2f\n", time_elapsed);
-//    cout << "Matrix A:" << endl << endl;
-//    A.printMatrix();
-//    cout << endl << endl << "Matrix B:" << endl << endl;
-//    B.printMatrix();
-//    cout << endl << endl << "Matrix C:" << endl << endl;
-//    C.printMatrix();
-
+    gettimeofday(&timecheck, NULL);
+    long timeofday_end = (long)timecheck.tv_sec * 1000 + (long)timecheck.tv_usec / 1000;
+   
+    time_elapsed = (timeofday_end - timeofday_start)/1000;
+    printf("Time elapsed %.4f\n", time_elapsed);
+   
+    cout << endl << endl << "Matrix C: " << endl;
+    for (int i = 0; i < MATRIX_SIZE; i++)
+    {
+        for (int j = 0; j < MATRIX_SIZE; j++)
+        {
+            //   cout << matrixC[i][j];
+        }
+        //  cout << endl;
+    }
 }
-//Output time here
-
-//Write output to file
-//Format output into columns
-//Add comments, error checking, exception handling
--- Module2/M2T1P_V2/nbproject/Makefile-Debug.mk
+++ Module2/M2T1P_V2/nbproject/Makefile-Debug.mk
@@ -35,7 +35,6 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
-	${OBJECTDIR}/Matrix.o \
 	${OBJECTDIR}/main.o
 
 
@@ -62,11 +61,6 @@ LDLIBSOPTIONS=
 ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/m2t1p_v2: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/m2t1p_v2 ${OBJECTFILES} ${LDLIBSOPTIONS}
-
-${OBJECTDIR}/Matrix.o: Matrix.cpp
-	${MKDIR} -p ${OBJECTDIR}
-	${RM} "$@.d"
-	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Matrix.o Matrix.cpp
 
 ${OBJECTDIR}/main.o: main.cpp
 	${MKDIR} -p ${OBJECTDIR}
